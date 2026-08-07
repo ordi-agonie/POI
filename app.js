13b2b1d7fde5692b00cacc6d4a00e2dd21caf0aa
ordi-agonie <dorian.gioe@gmail.com>
2026-08-07 10:27:27 +0200

    Create POI card from form input

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const bouton = document.getElementById("nouveau-poi");
 const formulaire = document.getElementById("formulaire-poi");
+const contenuPoi = document.getElementById("contenu-poi");
+const listePoi = document.getElementById("liste-poi");
+const boutonEnregistrer = document.getElementById("enregistrer-poi");
 
 function ouvrirFormulaire() {
   formulaire.hidden = false;
 }
 
+function enregistrerPoi() {
+  const texte = contenuPoi.value;
+
+  if (texte === "") {
+    return;
+  }
+
+  const nouveauPoi = document.createElement("article");
+
+  nouveauPoi.classList.add("poi");
+
+  nouveauPoi.innerHTML = `
+    <p class="poi-contenu">${texte}</p>
+  `;
+
+  listePoi.appendChild(nouveauPoi);
+
+  contenuPoi.value = "";
+}
+
 bouton.addEventListener("click",ouvrirFormulaire);
+boutonEnregistrer.addEventListener("click", enregistrerPoi);
